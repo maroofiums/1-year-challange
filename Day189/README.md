aebae7c2bcd9dafb872947e345b5ed8a033e8529
maroof2424 <maroof96965@gmail.com>
2025-10-06 20:33:04 +0500

    Day189

--- Day189/README.md
+++
@@ -1,3 +0,0 @@
-# Day 189
-
-Welcome to Day 189 of the 365 Days of Code Challenge!
---
+++ Day189✅/MiniStore/store/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    session_key = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.product.price * self.quantity
